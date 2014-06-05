9bbc7966e598513923dd5a9d3fe3fffa75cb0c3b
sun <rajender.chandel@sun-softwares.com>
2014-06-05 14:28:59 +0530

    first commit

---
+++ banner.php
@@ -0,0 +1,64 @@
+<div class="banner-bg"></div>
+ <div class="container">
+ 
+ 
+<div class="banner owl-carousel" id="slider" >
+<!--slide1 starts-->
+<div class="items">
+	<img src="<?php echo base_url(); ?>images/banner.png" alt="" class="slide_img">
+
+ 	<div class="banner_con">
+  <div class="row">
+   <div class="col-md-12">
+    <div class="banner-heading">
+     <h1 class="save">Save on the domain</h1> 
+     <h1 class="right">that's right for you</h1>
+     <p class="banner-matter">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the indus y text ever since the 1500s, when an unknown printer took a galley of type and scrambled it. Lorem Ipsum is simply dummy text of the printing</p>
+    </div>
+    <div class="arrow">
+     <a href="javascript:void(0);" onclick="move_prev();"><img style="margin-right:12px;" src="<?php echo base_url(); ?>images/left-arrow.png" alt="" ></a>
+     <a href="javascript:void(0);" onclick="move_next();"><img src="<?php echo base_url(); ?>images/right-arrow.png" alt=""></a>
+    </div>
+    
+    <div class="click-img">
+     <a href="#"><img src="<?php echo base_url(); ?>images/click-here-img.png" alt=""></a>
+    </div>
+    
+   </div>
+  </div>
+ </div>
+ </div>
+ <!--slide1 ends-->
+ 
+ <!--slide2 starts-->
+<div class="items">
+	<img src="<?php echo base_url(); ?>images/banner.png" alt="" class="slide_img">
+
+ 	<div class="banner_con">
+  <div class="row">
+   <div class="col-md-12">
+    <div class="banner-heading">
+     <h1 class="save">Save on the domain</h1> 
+     <h1 class="right">that's right for you</h1>
+     <p class="banner-matter">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the indus y text ever since the 1500s, when an unknown printer took a galley of type and scrambled it. Lorem Ipsum is simply dummy text of the printing</p>
+    </div>
+    <div class="arrow">
+     <a href="javascript:void(0);" onclick="move_prev();"><img style="margin-right:12px;" src="<?php echo base_url(); ?>images/left-arrow.png" alt="" ></a>
+     <a href="javascript:void(0);" onclick="move_next();"><img src="<?php echo base_url(); ?>images/right-arrow.png" alt=""></a>
+    </div>
+    
+    <div class="click-img">
+     <a href="#"><img src="<?php echo base_url(); ?>images/click-here-img.png" alt=""></a>
+    </div>
+    
+   </div>
+  </div>
+ </div>
+ </div>
+ <!--slide2 ends-->
+ 
+ </div>
+
+ 
+ 
+</div>
